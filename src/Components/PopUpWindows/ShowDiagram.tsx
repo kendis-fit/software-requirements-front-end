e2fdb6d3d8a9a9dda11e2307f5c0bf2d6734e781
Kurt Endless <kendis_it@hotmail.com>
2020-01-08 17:57:43 +0200

    update: add input colors range for diagram

--- src/Components/PopUpWindows/ShowDiagram.tsx
+++ src/Components/PopUpWindows/ShowDiagram.tsx
@@ -18,6 +18,9 @@ interface IShowDiagram
 const ShowDiagram = ({ Requirement }: IShowDiagram) => {
     
     const [isLoad, setIsLoad] = useState<boolean>(true);
+    const [dangerArea, setDangerArea] = useState<string>();
+    const [warningArea, setWarningArea] = useState<string>();
+    const [successArea, setSuccessArea] = useState<string>();
 
     const canvas = createRef<HTMLCanvasElement>();
 
@@ -31,12 +34,30 @@ const ShowDiagram = ({ Requirement }: IShowDiagram) => {
 
             if (!ctx) return;
 
+            const values = dataset.map(d => d.value);
+
+            const dangerValue = Number.parseFloat(dangerArea === undefined ? "" : dangerArea);
+            const warningValue = Number.parseFloat(warningArea === undefined ? "" : warningArea);
+            const successValue = Number.parseFloat(successArea === undefined ? "" : successArea);
+
             const marksData = {
                 labels: dataset.map(d => d.name),
                 datasets: [{
-                  label: "Requirement: test",
-                  backgroundColor: "rgb(128, 255, 223, 0.75)",
-                  data: dataset.map(d => d.value)
+                    label: "Requirement: test",
+                    backgroundColor: "rgb(35, 148, 209, 0.6)",
+                    data: values
+                    }, {
+                    label: "Danger",
+                    backgroundColor: "rgba(255, 0, 0, 0.6)",
+                    data: values.map(() => dangerValue)
+                    }, {
+                    label: "Warning",
+                    backgroundColor: "rgba(255, 217, 101, 0.6)",
+                    data: values.map(() => warningValue)
+                    }, {
+                    label: "Success",
+                    backgroundColor: "rgba(0, 176, 80, 0.6)",
+                    data: values.map(() => successValue)
                 }]
             };
                
@@ -61,7 +82,7 @@ const ShowDiagram = ({ Requirement }: IShowDiagram) => {
                 }
             })();
         }
-    }, [Requirement, canvas]);
+    }, [Requirement, canvas, dangerArea, warningArea, successArea]);
 
     return (
         <Form> 
@@ -70,7 +91,15 @@ const ShowDiagram = ({ Requirement }: IShowDiagram) => {
                 <ModalHeader data-close={false} Height="50px">Chart</ModalHeader>
                 <ModalBody data-close={false}>
                     {
-                        isLoad ? "Loading..." : <canvas data-close={false} style={{ background: "white" }} ref={canvas}></canvas>
+                        isLoad ? "Loading..." : 
+                            <>
+                                <div data-close={false} style={{ display: "flex", flexDirection: "row" }}>
+                                    <input style={{ width: "184px" }} data-close={false} value={dangerArea} onChange={e => setDangerArea(e.target.value)} placeholder="Max danger range" />
+                                    <input style={{ width: "184px" }} data-close={false} value={warningArea} onChange={e => setWarningArea(e.target.value)} placeholder="Max warning range" />
+                                    <input style={{ width: "184px" }} data-close={false} value={successArea} onChange={e => setSuccessArea(e.target.value)} placeholder="Max success range" />
+                                </div>
+                                <canvas data-close={false} style={{ background: "white" }} ref={canvas}></canvas>
+                            </>
                     }
                 </ModalBody>
             </Modal>
